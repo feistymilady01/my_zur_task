dd88c8073373394d5a0c4cf93d144b9fc9af856c
feistymilady01 <103441089+feistymilady01@users.noreply.github.com>
2022-06-09 12:43:21 +0100

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,2 +1,12 @@
-# Please istructor this is the correct link
-https://github.com/feistymilady01/Games
+# Please istructor the correct link to my Assignment is in the anchor tag.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>RSP</title>
+</head>
+<body>
+    <main>
+        <a href="https://github.com/feistymilady01/Games">Correct link to the assignment!</a>
+    </main>
+</body>
+</html>
